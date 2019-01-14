d61dd6b50cce592b16e46e6d3c130fbb0901cabf
anapaula <anapaula.ds.mendes@gmail.com>
2019-01-14 11:54:11 -0300

    Improve site sanploy.com.br

--- src/assets/js/signup.js
+++ src/assets/js/signup.js
@@ -1,6 +1,49 @@
 'use strict';
 import axios from 'axios';
 
+function fixString() {
+  var x = document.getElementById("subdomain");
+  x.value = x.value.toLowerCase().replace(/\s+/g, '');
+}
+
+function subdomainInvalid() {
+  var x = document.getElementById("subdomain");
+  x.setCustomValidity('Por favor, preencha este campo.');
+}
+
+function subdomainValid() {
+  var x = document.getElementById("subdomain");
+  x.setCustomValidity('');
+}
+
+function emailInvalid() {
+  var x = document.getElementById("email");
+  x.setCustomValidity('Por favor, preencha com seu e-mail.');
+}
+
+function emailValid() {
+  var x = document.getElementById("email");
+  x.setCustomValidity('');
+}
+
+function termInvalid() {
+  var x = document.getElementById("terms");
+  x.setCustomValidity('Para continuar você precisa concordar com os Termos e Condições.');
+}
+
+function termValid() {
+  var x = document.getElementById("terms");
+  x.setCustomValidity('');
+}
+
+document.getElementById("subdomain").addEventListener("keyup", fixString);
+document.getElementById("subdomain").addEventListener("invalid", subdomainInvalid);
+document.getElementById("subdomain").addEventListener("change", subdomainValid);
+document.getElementById("email").addEventListener("invalid", emailInvalid);
+document.getElementById("email").addEventListener("change", emailValid);
+document.getElementById("terms").addEventListener("invalid", termInvalid);
+document.getElementById("terms").addEventListener("change", termValid);
+
 var form = document.querySelector("form");
 form.addEventListener("submit", function(evt){
   var error = false, data = {};
@@ -13,17 +56,19 @@ form.addEventListener("submit", function(evt){
 			  error = true;
 			} else {
 				el.style["border-color"] = "#c6eaf7";
-				data[el.name] = el.value;
+        data[el.name] = el.value;
 			}
 	});
+  delete data[""];
+
 	if (!error) {
     return axios({
       method: 'post',
-      url: "https://app.dev.quiploy.com/users/",
+      url: "",
       data: data,
     })
     .then(function (response) {
-      form.reset();
-    });
+        form.reset();
+    })
   }
 });
